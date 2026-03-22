b27498a4f04078e7b66cee0a7675d06c595e8108
Utkarsh Mishra <utkarsh.e.e.mishra@gmail.com>
2026-03-23 00:45:56 +0530

    Refactor main.c for improved readability and functionality

--- main.c
+++ main.c
@@ -6,10 +6,9 @@
 #include <stdlib.h>
 #include <errno.h>
 
-
-
 #define MAX_ARGS 64
 #define DELIM " \t\r\n\a"
+
 
 void parse_to_argv(char *line, char *argv[])
 {
@@ -30,13 +29,14 @@ void parse_to_argv(char *line, char *argv[])
 
 int main()
 {
-	
-
 	while(1)
 	{
-		char line[125];
+		char line[1025];
 		char *argv[MAX_ARGS];
+		char *home_path_value = getenv("HOME");
+
 		printf("lucidsh > ");
+		fflush(stdout);
 
 		if (fgets(line, sizeof(line), stdin) == NULL)
 		{
@@ -48,9 +48,39 @@ int main()
 
 		if (argv[0] == NULL)
 			continue;
+	
 
 		if (strcmp(argv[0], "exit") == 0)
-				exit(0);
+			exit(0);
+
+		if(strcmp(argv[0], "cd") == 0)
+		{
+			if (argv[1] == NULL)
+			{
+				if (home_path_value == NULL)
+				{
+					printf("Home not set\n");
+				}
+				
+				else if (chdir(home_path_value) == -1)
+				{
+					printf("chdir failed");
+				}
+			}
+			else
+			{
+				if (chdir(argv[1]) == -1)
+				{
+					perror("chdir failed");
+					
+				}
+			}
+			continue;
+		}
+
+
+
+		
 
 	pid_t pid = fork();
 
@@ -60,11 +90,10 @@ int main()
 			exit(1);
 		}
 
-		else if (pid ==0)
+		else if (pid == 0)
 		{
 			//Child Process
-
-			if (execvp(argv[0], argv) == -1)
+			if ((execvp(argv[0], argv)) == -1)
 			{
 				perror("execvp failed");
 				exit(1);
@@ -77,11 +106,6 @@ int main()
 			int status;
 			waitpid(pid, &status, 0);
 		}
-
-		
-
 	}
-
-
 	return 0;
 }
